10c6ecbeacc20baf319c8cd5bd94a6ca0c6f40ee
Khaled FERJANI <kferjani@linagora.com>
2024-07-16 14:14:28 +0100

    ✨ feat: added button components

---
+++ src/lib/components/buttons/SubmitButton.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Spinner from '$components/icons/SpinnerIcon.svelte';
+
+	export let disabled: boolean = false;
+	export let ariaLabel: string = '';
+	export let loading: boolean = false;
+</script>
+
+<button
+	type="submit"
+	aria-label={ariaLabel}
+	{disabled}
+	class="flex items-center rounded justify-center w-full h-12 {disabled
+		? 'bg-slate-200 text-slate-400'
+		: 'bg-primary text-white'} text-center text-sm not-italic font-medium leading-5 tracking-[0.1px] space-x-5"
+>
+	<slot />
+	{#if loading}
+		<span class="flex items-center px-1 text-white">
+			<Spinner />
+		</span>
+	{/if}
+</button>
