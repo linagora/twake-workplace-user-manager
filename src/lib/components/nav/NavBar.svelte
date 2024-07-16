eceb1a332115107fdc1bcf0c4bdb65e1591c63e0
Khaled FERJANI <kferjani@linagora.com>
2024-07-16 16:46:14 +0100

    ✨ feat: added Nav bar component

---
+++ src/lib/components/nav/NavBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import LogoutIcon from '$components/icons/LogoutIcon.svelte';
+	import TwpSmallLogo from '$components/logos/TwpSmallLogo.svelte';
+	import ConnectedUserDisplay from '$components/user/ConnectedUserDisplay.svelte';
+	import { connectedUser } from '$store';
+</script>
+
+<nav class="bg-gray-300/30 h-20 w-full p-5 shadow-xl flex">
+	<div class="flex-1">
+		<TwpSmallLogo />
+	</div>
+	<div class="flex-0 flex gap-2 items-center justify-center">
+		<ConnectedUserDisplay name={$connectedUser.cn} />
+		<button type="button" class="hover:bg-gray-400/30  p-2 rounded-full">
+			<LogoutIcon />
+		</button>
+	</div>
+</nav>
