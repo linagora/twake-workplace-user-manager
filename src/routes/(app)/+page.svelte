c45136dfeb2498e38779316e06566d74fad8a16b
Khaled FERJANI <kferjani@linagora.com>
2024-07-16 15:40:29 +0100

    ✨ feat: fetch the currently connected user

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { connectedUser } from '$store/user';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: ({ currentUser, usersList } = data);
+	$: connectedUser.set(currentUser);
+</script>
+
+<pre>
+
+  {#each usersList as user}
+		{user.cn}
+	{/each}
+  
+</pre>
