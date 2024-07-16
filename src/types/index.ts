5cdd262dec5d09937259cebeceab360870efeae2
Khaled FERJANI <kferjani@linagora.com>
2024-07-16 10:58:59 +0100

    🏷️ chore: added auth related types

--- src/types/index.ts
+++ src/types/index.ts
@@ -9,3 +9,39 @@ export interface ILdapClient {
 }
 
 export type SearchResult = Record<string, string | string[] | number>;
+export interface IAuthService {
+	fetchToken: () => Promise<string | undefined>;
+	login: (user: string, password: string) => Promise<string>;
+	logout: (cookie: string) => Promise<void>;
+	validateToken: (cookie: string) => Promise<boolean>;
+	fetchConnectedUser: (cookie: string) => Promise<string | undefined>;
+}
+
+export interface AuthAPIResponse {
+	error?: string;
+	result: number;
+}
+
+export interface AuthResponse extends AuthAPIResponse {
+	id: string;
+}
+
+export interface TokenResponse extends AuthAPIResponse {
+	token: string;
+}
+
+export interface User {
+	uid: string;
+	cn: string;
+	sn: string;
+	givenName: string;
+	userPassword: string;
+	mobile: string;
+	displayName?: string;
+	mail?: string;
+	objectclass: 'inetOrgPerson';
+}
+
+export interface SessionResponse extends Omit<AuthAPIResponse, 'result'> {
+	result: 0 | 'string';
+}
