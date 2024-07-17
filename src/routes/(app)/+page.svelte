a477a4138baafc71f4511b8b5a62e2b41a094f48
Khaled FERJANI <kferjani@linagora.com>
2024-07-17 15:35:14 +0100

    🎨 chore: update main page

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import UsersList from '$components/lists/UsersList.svelte';
+	import { toggleForm } from '$store/form';
 	import { connectedUser } from '$store/user';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	$: ({ currentUser, usersList } = data);
+	$: ({ currentUser, usersList, form } = data);
 	$: connectedUser.set(currentUser);
+	$: toggleForm.set(form);
 </script>
 
-<div class="p-5 bg-white rounded shadow-xl w-full lg:px-10">
+<div class="py-5 bg-white rounded shadow-2xl w-full lg:px-2">
 	<UsersList users={usersList} />
 </div>
