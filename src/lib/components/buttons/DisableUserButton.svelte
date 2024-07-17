1e7c7e4ee146a090ae6971e4d904d9d6e229ffa0
Khaled FERJANI <kferjani@linagora.com>
2024-07-17 15:34:13 +0100

    ✨ feat: added toggle user buttons

---
+++ src/lib/components/buttons/DisableUserButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import DisableIcon from '$components/icons/DisableIcon.svelte';
+import SpinnerIcon from '$components/icons/SpinnerIcon.svelte';
+	import { t } from 'svelte-i18n';
+
+	export let loading: boolean = false;
+	export let disabled: boolean = false;
+	export let handler: () => void = () => {};
+</script>
+
+<button
+	{disabled}
+	on:click={handler}
+	type="button"
+	class="inline-flex gap-2 items-center px-4 py-2 text-sm font-medium {disabled
+		? 'text-white bg-slate-500'
+		: 'text-gray-900 bg-white hover:bg-gray-100 cursor-pointer shadow'}"
+>
+  <DisableIcon />
+	{$t('disable-user')}
+	{#if loading}
+		<SpinnerIcon />
+	{/if}
+</button>
