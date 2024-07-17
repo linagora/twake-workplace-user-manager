cc6941457d12d891d7c2a2e0024773b141e8db04
Khaled FERJANI <kferjani@linagora.com>
2024-07-17 15:30:09 +0100

    🎨 chore: added pwdAccountLockedTime to user attributes

--- src/lib/utils/user.ts
+++ src/lib/utils/user.ts
@@ -1,4 +1,4 @@
 /**
  * the list of the user attributes in LDAP
  */
-export const userAttributes = ['cn', 'sn', 'givenName', 'mobile', 'mail'];
+export const userAttributes = ['dn', 'cn', 'sn', 'givenName', 'mobile', 'mail', 'pwdAccountLockedTime'];
