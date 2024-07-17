53a0e6558221157bfea5000f48f77a43c3133bc2
Khaled FERJANI <kferjani@linagora.com>
2024-07-17 15:31:49 +0100

    💄 fix: connected user dispaly ui

--- src/lib/components/user/ConnectedUserDisplay.svelte
+++ src/lib/components/user/ConnectedUserDisplay.svelte
@@ -4,7 +4,7 @@
 	export let name: string;
 </script>
 
-<div class="flex items-center justify-center gap-1 hover:bg-gray-400/30 rounded-full px-5 transition duration-450">
+<div class="flex items-center justify-center gap-1 hover:bg-gray-400/30 rounded-full px-5 transition duration-450 cursor-pointer">
 	<UserIcon />
 
 	{name}
