c7acbe2fea329be9089bec4ab616d8d080f1a395
Khaled FERJANI <kferjani@linagora.com>
2024-07-26 09:30:08 +0100

    💄 fix: action buttons layouts

--- src/lib/components/buttons/DisableUserButton.svelte
+++ src/lib/components/buttons/DisableUserButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import DisableIcon from '$components/icons/DisableIcon.svelte';
-import SpinnerIcon from '$components/icons/SpinnerIcon.svelte';
+	import SpinnerIcon from '$components/icons/SpinnerIcon.svelte';
 	import { t } from 'svelte-i18n';
 
 	export let loading: boolean = false;
@@ -12,13 +12,14 @@ import SpinnerIcon from '$components/icons/SpinnerIcon.svelte';
 	{disabled}
 	on:click={handler}
 	type="button"
-	class="inline-flex gap-2 items-center px-4 py-2 text-sm font-medium {disabled
+	class="flex gap-2 items-center w-full max-w-[130px] justify-center px-4 py-2 text-sm font-medium {disabled
 		? 'text-white bg-slate-500'
 		: 'text-gray-900 bg-white hover:bg-gray-100 cursor-pointer shadow'}"
 >
-  <DisableIcon />
-	{$t('disable-user')}
 	{#if loading}
 		<SpinnerIcon />
+	{:else}
+		<DisableIcon />
 	{/if}
+	{$t('disable-user')}
 </button>
--- src/lib/components/buttons/EnableUserButton.svelte
+++ src/lib/components/buttons/EnableUserButton.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import EnableIcon from '$components/icons/EnableIcon.svelte';
-import SpinnerIcon from '$components/icons/SpinnerIcon.svelte';
+	import SpinnerIcon from '$components/icons/SpinnerIcon.svelte';
 	import { t } from 'svelte-i18n';
 
 	export let disabled: boolean = false;
@@ -12,13 +12,14 @@ import SpinnerIcon from '$components/icons/SpinnerIcon.svelte';
 	{disabled}
 	on:click={handler}
 	type="button"
-	class="inline-flex gap-2 items-center px-4 py-2 text-sm font-medium {disabled
+	class="flex gap-2 items-center w-full max-w-[130px] justify-center px-4 py-2 text-sm font-medium {disabled
 		? 'text-white bg-slate-500'
 		: 'text-gray-900 bg-white hover:bg-gray-100 cursor-pointer shadow-xl'}"
 >
-  <EnableIcon />
-	{$t('enable-user')}
 	{#if loading}
 		<SpinnerIcon />
+	{:else}
+		<EnableIcon />
 	{/if}
+	{$t('enable-user')}
 </button>
