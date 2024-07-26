b516980a33f7811b0fd314df0364338e3200b27c
Khaled FERJANI <kferjani@linagora.com>
2024-07-26 09:29:04 +0100

    🍱 chore: update assets

--- src/lib/components/icons/EnableIcon.svelte
+++ src/lib/components/icons/EnableIcon.svelte
@@ -1,4 +1,4 @@
-<svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg class="w-6 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 	<path
 		d="M7 12L9.89075 14.8907V14.8907C9.95114 14.951 10.049 14.9511 10.1094 14.8907V14.8907L17 8"
 		stroke="currentColor"
--- src/lib/components/icons/SpinnerIcon.svelte
+++ src/lib/components/icons/SpinnerIcon.svelte
@@ -1,6 +1,6 @@
 <svg
 	aria-hidden="true"
-	class="w-4 h-4 me-2 text-gray-200 animate-spin fill-gray-600"
+	class="w-4 h-4 text-gray-200 animate-spin fill-gray-600"
 	viewBox="0 0 100 101"
 	fill="none"
 	xmlns="http://www.w3.org/2000/svg"
