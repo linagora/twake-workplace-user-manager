3afd8685dc5d1ccdd2ad3b0739042e78ba7606c7
Khaled FERJANI <kferjani@linagora.com>
2024-07-30 14:17:32 +0100

    ✨ feat: added logout user component

---
+++ src/lib/components/user/LogoutUser.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import LogoutIcon from '$components/icons/LogoutIcon.svelte';
+</script>
+
+<a href="/logout" class="hover:bg-gray-400/30 p-2 rounded-full transition duration-450">
+	<LogoutIcon />
+</a>
