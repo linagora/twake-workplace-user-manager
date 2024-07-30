bb44fb72e19b46127ba7cfe2ed089c7cccbed930
Khaled FERJANI <kferjani@linagora.com>
2024-07-30 13:54:48 +0100

    ✨ feat: add enable/disable account Attribute helpers

--- src/lib/utils/user.ts
+++ src/lib/utils/user.ts
@@ -10,3 +10,24 @@ export const userAttributes = [
 	'mail',
 	'pwdAccountLockedTime'
 ];
+
+/**
+ * LDAP modification attribute to disable a user
+ * Holds the time that the user's account was locked.
+ * A locked account means that the password may no longer be used to authenticate.
+ * A value of 000001010000Z means the account has been locked permanently, and that only a password administrator can unlock the account.
+ */
+export const disableUserAttribute = {
+	type: 'pwdAccountLockedTime',
+	values: ['000001010000Z']
+};
+
+/**
+ * LDAP modification attribute to enable a user
+ * Removes the pwdAccountLockedTime attribute, effectively enabling the user's account.
+ * @see disableUserAttribute
+ */
+export const enableUserAttribute = {
+	type: 'pwdAccountLockedTime',
+	values: []
+};
